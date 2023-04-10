5a61a4350669e67b7617a2eaf509dbe4c07ebdce
roberto <robertointerface@gmail.com>
2023-04-10 17:06:57 +0100

    Load database credentials by secrets

--- lambdas/check_product_availability/check_product_availability_lambda/database_connection.py
+++ lambdas/check_product_availability/check_product_availability_lambda/database_connection.py
@@ -1,13 +1,15 @@
+import json
 import os
 from abc import ABC, abstractmethod
 import pymongo
-import urllib
-# from boto3 import Session
-# from botocore.credentials import ReadOnlyCredentials
+from check_product_availability_lambda.utils import get_string_secret
 
 MONGO_CONNECTION = None
 MONGO_CONNECTION_PORT = 27017
 PAYMENT_SYSTEM_DATABASE_NAME = os.environ['PAYMENT_SYSTEM_DATABASE_NAME']
+DATABASE_USERNAME_SECRET = os.environ['DATABASE_USERNAME_SECRET']
+DATABASE_PASSWORD_SECRET = os.environ['DATABASE_PASSWORD_SECRET']
+DATABASE_CLUSTER_SECRET = os.environ['DATABASE_CLUSTER_SECRET']
 
 
 class MongoDbConnection(ABC):
@@ -24,7 +26,14 @@ class MongoDbLocalConnection(MongoDbConnection):
 
 
 def connect_to_mongo():
-    connection_string = f"mongodb+srv://User_name:Password$@Cluster_Name.mmdvvxm.mongodb.net/?retryWrites=true&w=majority"
+    user_name = json.loads(get_string_secret(secret_name=DATABASE_USERNAME_SECRET,
+                                             region_name="eu-west-2"))['DATABASE_USERNAME_SECRET']
+    password = json.loads(get_string_secret(secret_name=DATABASE_PASSWORD_SECRET,
+                                 region_name="eu-west-2"))['DATABASE_PASSWORD_SECRET']
+    cluster_name = json.loads(get_string_secret(secret_name=DATABASE_CLUSTER_SECRET,
+                                     region_name="eu-west-2"))['DATABASE_CLUSTER_SECRET']
+    connection_string = f"mongodb+srv://{user_name}:{password}" \
+                        f"@{cluster_name}.mmdvvxm.mongodb.net/?retryWrites=true&w=majority"
     client = pymongo.MongoClient(connection_string)
     return client
 
--- lambdas/check_product_availability/check_product_availability_lambda/utils.py
+++ lambdas/check_product_availability/check_product_availability_lambda/utils.py
@@ -1,7 +1,4 @@
-from typing import Union
-
 import boto3
-import base64
 from botocore.exceptions import ClientError
 
 
